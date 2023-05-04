45889c4d2e8d1698277c62f7fa54769fdc3292c5
DavydKuzikevich <wliorjroo@gmail.com>
2023-05-04 10:44:26 +0300

    feat: implement subtask `02-write-file`

---
+++ 02-write-file/index.js
@@ -0,0 +1,42 @@
+import { createInterface } from 'node:readline';
+import { createWriteStream } from 'node:fs';
+import os from 'node:os';
+const main = async () => {
+  try {
+    process.once('exit', () => {
+      console.log(`${os.EOL}Thank you for using Node.js, goodbye!`);
+    });
+    console.log(`Welcome to the "Write file"!`);
+
+    const outStream = createWriteStream('02-write-file/02-write-file.txt');
+
+    const rl = createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.on('line', async (data) => {
+      try {
+        if (data) {
+          switch (data) {
+            case 'exit':
+              outStream.close();
+              process.exit();
+            default:
+              outStream.write(data + os.EOL);
+              break;
+          }
+        }
+      } catch (err) {
+        console.log(err.message);
+        outStream.close();
+        process.exit();
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+    process.exit();
+  }
+}
+
+main();
