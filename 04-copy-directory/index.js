5b2428c75f4916b3d56019154480e75b59c79c09
DavydKuzikevich <wliorjroo@gmail.com>
2023-05-04 11:53:13 +0300

    feat: implement subtask `04-copy-directory`

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { opendir, mkdir } from 'node:fs/promises'
+import { pipeline } from 'node:stream/promises';
 
+const copyFile = async (src, dest) => {
+  try {
+    return pipeline(createReadStream(src), createWriteStream(dest));
+  } catch (err) {
+    throw errOperationFailed;
+  }
+};
+
+const copyDir = async (srcPath, destPath) => {
+  await mkdir(destPath, { recursive: true });
+  const dir = await opendir(srcPath);
+  for await (const dirent of dir) {
+    if (dirent.isFile()) {
+      copyFile(srcPath + dirent.name, destPath + dirent.name);
+    } else if (dirent.isDirectory()) {
+      copyDir(srcPath + dirent.name + '/', destPath + dirent.name + '/');
+    }
+  }
+}
+
+copyDir('./04-copy-directory/files/', './04-copy-directory/files-copy/');
