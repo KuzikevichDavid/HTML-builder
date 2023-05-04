b063919365f201152d39cdee5ba87887db6d5677
DavydKuzikevich <wliorjroo@gmail.com>
2023-05-04 10:25:15 +0300

    feat: implement subtask `01-read-file`

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+import { createReadStream } from 'node:fs'
+import { pipeline } from 'node:stream/promises';
+
+const read = async (outStream, path) => {
+  try {
+    return pipeline(createReadStream(path), outStream);
+  } catch (err) {
+    throw new Error('Operation failed');
+  }
+};
+
+read(process.stdout, './01-read-file/text.txt');
